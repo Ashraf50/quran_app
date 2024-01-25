73a5570e220ac42ae14268991c13552a20143fdb
Ashraf50 <ashrafesam00@gmail.com>
2024-01-25 14:36:14 +0200

    some editing on details page

--- lib/feature/Details/presentation/view/details_view_body.dart
+++ lib/feature/Details/presentation/view/details_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:quran_app/core/utils/text_style.dart';
 import 'package:quran_app/core/widget/custom_app_bar.dart';
+import 'package:quran_app/feature/Details/presentation/view/surah_details_widget.dart';
 
 class DetailsViewBody extends StatelessWidget {
   const DetailsViewBody({super.key});
@@ -24,55 +24,7 @@ class DetailsViewBody extends StatelessWidget {
                 Navigator.pop(context);
               },
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Stack(
-                children: [
-                  Image.asset("assets/img/details background.png"),
-                  const Positioned(
-                    top: 70,
-                    left: 110,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Al-Fatiah",
-                          style: Styles.textStyle26,
-                        ),
-                        Text(
-                          "The opening",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 40,
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "MECCAN ",
-                              style: Styles.textStyle14,
-                            ),
-                            Text(
-                              "- 7 VERSES",
-                              style: Styles.textStyle14,
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 60,
-                        ),
-                        Text(
-                          "بسم الله الرحمن الرحيم",
-                          style: Styles.textStyle24,
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
+            const SurahDetailsWidget(),
           ],
         ),
       ),
---
+++ lib/feature/Details/presentation/view/surah_details_widget.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/core/utils/text_style.dart';
+
+class SurahDetailsWidget extends StatelessWidget {
+  const SurahDetailsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Stack(
+        children: [
+          Image.asset("assets/img/details background.png"),
+          const Positioned(
+            top: 70,
+            left: 110,
+            child: Column(
+              children: [
+                Text(
+                  "Al-Fatiah",
+                  style: Styles.textStyle26,
+                ),
+                Text(
+                  "The opening",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "MECCAN ",
+                      style: Styles.textStyle14,
+                    ),
+                    Text(
+                      "- 7 VERSES",
+                      style: Styles.textStyle14,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                Text(
+                  "بسم الله الرحمن الرحيم",
+                  style: Styles.textStyle24,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
